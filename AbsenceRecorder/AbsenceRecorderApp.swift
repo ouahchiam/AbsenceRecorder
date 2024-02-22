9b5f56cc12d83577e34c1908b53a01a82ed5a14a
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-02-22 18:26:58 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Ouahchia, Mohamed (JD) on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
