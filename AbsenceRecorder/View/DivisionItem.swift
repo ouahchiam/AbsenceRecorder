c6229361a8819961e930a9386b47c0fb45c7694b
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-02-22 19:44:06 +0000

    Added list for divisions

---
+++ AbsenceRecorder/View/DivisionItem.swift
@@ -0,0 +1,18 @@
+//
+//  DivisionItem.swift
+//  AbsenceRecorder
+//
+//  Created by Ouahchia, Mohamed (JD) on 22/02/2024.
+//
+
+import SwiftUI
+
+struct DivisionItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisionItem()
+}
