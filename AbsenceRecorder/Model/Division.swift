012251b45720b1bc3f780ffe84841e4dec66b5e5
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-02-22 18:55:34 +0000

    Added examples for divisons (auto generated)

--- AbsenceRecorder/Model/Division.swift
+++ AbsenceRecorder/Model/Division.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+func randomAlphanumericString(_ length: Int) -> String {
+   let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+   let randomString = (0..<length).map{ _ in String(letters.randomElement()!) }.reduce("", +)
+   return randomString
+}
+
+class Division {
+    let code: String
+    var students: [String] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+    
+    #if DEBUG
+    static func createDivision(code: String, of size: Int) -> Division {
+        let division = Division(code: code)
+        
+        var students: [Student] = []
+        
+        for i in 0..<size {
+            students.append(Student(forename: randomAlphanumericString(Int.random(in: 0..<7)), surname: randomAlphanumericString(Int.random(in: 0..<7)), dob: Date(timeIntervalSince1970: 3155760000)))
+        }
+        
+        return division
+    }
+    
+    static let examples = [Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8),
+                           Division.createDivision(code: ("\(randomAlphanumericString(3))-\(Int.random(in: 0..<9))"), of: 8)]
+    #endif
+}
+
--- AbsenceRecorder/Model/Student.swift
+++ AbsenceRecorder/Model/Student.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class Student {
+    let forename: String
+    let surname: String
+    let dob: Date
+    
+    init(forename: String, surname: String, dob: Date) {
+        self.forename = forename
+        self.surname = surname
+        self.dob = dob
+    }
+}
